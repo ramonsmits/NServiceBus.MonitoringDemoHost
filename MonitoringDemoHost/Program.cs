175340fe23fbb6ff89725afeed3c8736092299e4
Ramon Smits <ramon.smits@gmail.com>
2017-09-29 11:06:31 +0200

    A bit of gold plating.

--- MonitoringDemoHost/Program.cs
+++ MonitoringDemoHost/Program.cs
@@ -8,26 +8,32 @@ using NServiceBus.Logging;
 
 class Program
 {
+    static TimeSpan MetricsReportingInterval = TimeSpan.FromSeconds(2.5);
+    static int EndpointSize = 25;
+    static int InstanceModulo = 4;
+    static int RecoverabilityImmediateRetryCount = 1;
+    static int RecoverabilityDelayedRetryCount = 5;
+    static TimeSpan RecoverabilityDelayedRetryBackoffIncrement = TimeSpan.FromSeconds(1);
+    static bool AuditForwardingEnabled = false;
+
     public static Tuple<string, IEndpointInstance>[] Instances;
     static async Task Main()
     {
         LogManager.Use<DefaultFactory>().Level(LogLevel.Error);
-        var endpointSize = 25;
-        var instanceModulo = 4;
 
-        var providers = new[] { "Payment", "Audit", "Approval", "Ledger", "Integration", "Broadcast", "Notification", "Identity", "Tax", "Exchange" };
-        var types = new[] { "Service", "Gateway", "Adapter", "Translator", "Provider" };
+        var providers = new[] { "Payment", "Audit", "Approval", "Ledger", "Integration", "Broadcast", "Notification", "Identity", "Report", "Exchange" };
+        var types = new[] { "Service", "Gateway", "Adapter", "Translator", "Provider", "Generator" };
         var ou = new[] { "Sales", "Transport", "Accounting", "Marketing", "Support", "Development", "Research", "HumanResourceManagement", "Production", "Purchasing" };
 
         var random = new Random(1337);
 
         Console.WriteLine("Creating instances");
         var tasks = new List<Task<Tuple<string, IEndpointInstance>>>();
-        for (int i = 0; i < endpointSize; ++i)
+        for (int i = 0; i < EndpointSize; ++i)
         {
             var endpointName = $"$ParticularLabs.{ou[random.Next(ou.Length)]}.{providers[random.Next(providers.Length)]}{types[random.Next(types.Length)]}";
             await Console.Out.WriteAsync($"Initializing {endpointName}").ConfigureAwait(false);
-            for (int j = 0; j < i % instanceModulo + 1; j++)
+            for (int j = 0; j < i % InstanceModulo + 1; j++)
             {
                 await Console.Out.WriteAsync(".").ConfigureAwait(false);
                 tasks.Add(Create(endpointName, j));
@@ -54,13 +60,13 @@ class Program
         var cfg = new EndpointConfiguration(name);
         //cfg.MakeInstanceUniquelyAddressable(instance.ToString("000"));
         if (Debugger.IsAttached) cfg.EnableInstallers();
-        //cfg.ForwardReceivedMessagesTo("audit");
+        if(AuditForwardingEnabled) cfg.ForwardReceivedMessagesTo("audit");
         cfg.UsePersistence<InMemoryPersistence>();
         cfg.SendFailedMessagesTo("error");
 
         cfg.Recoverability()
-            .Immediate(c => c.NumberOfRetries(1))
-            .Delayed(c => c.NumberOfRetries(5).TimeIncrease(TimeSpan.FromSeconds(1)));
+            .Immediate(c => c.NumberOfRetries(RecoverabilityImmediateRetryCount))
+            .Delayed(c => c.NumberOfRetries(RecoverabilityDelayedRetryCount).TimeIncrease(RecoverabilityDelayedRetryBackoffIncrement));
 
         cfg.RegisterComponents(x => x.ConfigureComponent<DelayHandler>(DependencyLifecycle.SingleInstance));
         cfg.RegisterComponents(x => x.ConfigureComponent<ChaosHandler>(DependencyLifecycle.SingleInstance));
@@ -71,13 +77,13 @@ class Program
         });
         cfg.LimitMessageProcessingConcurrencyTo(4 * Environment.ProcessorCount);
         var transport = cfg.UseTransport<MsmqTransport>();
-        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive); // Lower transaction mode to prevent transaction issues with MSDTC.
         transport.ApplyLabelToMessages(headers => (headers.ContainsKey(Headers.EnclosedMessageTypes) ? headers[Headers.EnclosedMessageTypes].Substring(0, Math.Min(200, headers[Headers.EnclosedMessageTypes].Length)) + "@" : string.Empty) + DateTime.UtcNow.ToString("O"));
 
 #pragma warning disable 618
         cfg.EnableMetrics().SendMetricDataToServiceControl(
             "Particular.ServiceControl.Monitoring",
-            TimeSpan.FromSeconds(2.5),
+            MetricsReportingInterval,
             instance.ToString("000")
             );
 #pragma warning restore 618
