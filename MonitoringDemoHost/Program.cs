b17bcb0273ac298f40ea01973a08eca73c2fd2e3
Ramon Smits <ramon.smits@gmail.com>
2017-10-06 21:59:47 +0200

    c#7 tuples, writing start duration to console & enabling performance counters.

--- MonitoringDemoHost/Program.cs
+++ MonitoringDemoHost/Program.cs
@@ -20,8 +20,10 @@ class Program
     static LogLevel LogLevel = LogLevel.Error;
     static TimeSpan HeartbeatInterval = MetricsReportingInterval;
     static TimeSpan HeartbeatTTL = TimeSpan.FromTicks(HeartbeatInterval.Ticks* 4);
+    static string HeartbeatQueue = "Particular.ServiceControl";
+    static string MonitoringQueue = HeartbeatQueue + ".Monitoring";
 
-    public static Tuple<string, IEndpointInstance>[] Instances;
+    public static (string name, IEndpointInstance instance)[] Instances;
     static async Task Main()
     {
         if (Environment.UserInteractive) Console.Title = "Monitoring demo host";
@@ -35,7 +37,8 @@ class Program
         var random = new Random(1337); // Makes sure that random endpoints names are repeatable
 
         Console.WriteLine("Creating instances");
-        var tasks = new List<Task<Tuple<string, IEndpointInstance>>>();
+        var start = Stopwatch.StartNew();
+        var tasks = new List<Task<(string name, IEndpointInstance)>>();
         for (int i = 0; i < EndpointSize; ++i)
         {
             var endpointName = $"$ParticularLabs.{ou[random.Next(ou.Length)]}.{providers[random.Next(providers.Length)]}{types[random.Next(types.Length)]}";
@@ -48,8 +51,9 @@ class Program
             await Console.Out.WriteLineAsync().ConfigureAwait(false);
         }
 
-        await Console.Out.WriteLineAsync("Starting...").ConfigureAwait(false);
+        await Console.Out.WriteAsync("Starting...").ConfigureAwait(false);
         Instances = await Task.WhenAll(tasks).ConfigureAwait(false);
+        await Console.Out.WriteAsync($"Done! Took {start.Elapsed} to start {Instances.Length} instances.").ConfigureAwait(false);
         await Console.Out.WriteLineAsync("Press ESC to exit...").ConfigureAwait(false);
 
         while (Console.ReadKey().Key != ConsoleKey.Escape)
@@ -57,11 +61,11 @@ class Program
         }
 
         await Console.Out.WriteLineAsync("Stopping...").ConfigureAwait(false);
-        await Task.WhenAll(Instances.Select(x => x.Item2.Stop())).ConfigureAwait(false);
+        await Task.WhenAll(Instances.Select(x => x.instance.Stop())).ConfigureAwait(false);
         await Console.Out.WriteLineAsync("Stopped!").ConfigureAwait(false);
     }
 
-    static async Task<Tuple<string, IEndpointInstance>> Create(string name, int instance)
+    static async Task<(string name, IEndpointInstance)> Create(string name, int instance)
     {
         await Task.Yield();
         var instanceSuffix = instance.ToString("000");
@@ -84,6 +88,8 @@ class Program
             return Task.CompletedTask;
         });
         cfg.LimitMessageProcessingConcurrencyTo(4 * Environment.ProcessorCount);
+
+
         var transport = cfg.UseTransport<MsmqTransport>();
         transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive); // Lower transaction mode to prevent transaction issues with MSDTC.
         transport.ApplyLabelToMessages(headers => (headers.ContainsKey(Headers.EnclosedMessageTypes) ? headers[Headers.EnclosedMessageTypes].Substring(0, Math.Min(200, headers[Headers.EnclosedMessageTypes].Length)) + "@" : string.Empty) + DateTime.UtcNow.ToString("O"));
@@ -92,18 +98,21 @@ class Program
             ? Guid.NewGuid()
             : GuidUtility.Create(NamespaceIdentifier, $"{name}-{instanceSuffix}");
 
+#pragma warning disable CS0618 // Type or member is obsolete
+        cfg.EnableCriticalTimePerformanceCounter();
+#pragma warning restore CS0618 // Type or member is obsolete
         cfg.UniquelyIdentifyRunningInstance().UsingCustomIdentifier(hostId);
 
 #pragma warning disable 618
         cfg.EnableMetrics().SendMetricDataToServiceControl(
-            "Particular.ServiceControl.Monitoring",
+            MonitoringQueue,
             MetricsReportingInterval,
             instance.ToString("000")
             );
 #pragma warning restore 618
 
-        cfg.HeartbeatPlugin("particular.servicecontrol", HeartbeatInterval, HeartbeatTTL);
+        cfg.HeartbeatPlugin(HeartbeatQueue, HeartbeatInterval, HeartbeatTTL);
 
-        return Tuple.Create(name, await Endpoint.Start(cfg).ConfigureAwait(false));
+        return (name, await Endpoint.Start(cfg).ConfigureAwait(false));
     }
 }
