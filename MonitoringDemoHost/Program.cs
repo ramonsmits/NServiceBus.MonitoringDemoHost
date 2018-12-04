d748a05885d29bfadde6a1d9e68c345e67a34f2a
Ramon Smits <ramon.smits@gmail.com>
2018-12-04 13:21:54 +0100

    Upgraded to nservicebus core v7 and made a lot of settings configurable via config.

--- MonitoringDemoHost/Program.cs
+++ MonitoringDemoHost/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Runtime;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
+using SelfTest;
+using SelfTest.Messages;
 
 class Program
 {
+    private static NameValueCollection AppSettings = ConfigurationManager.AppSettings;
     static Guid NamespaceIdentifier = GuidUtility.Create(Guid.Empty, "monitoringdemohost");
-    static TimeSpan MetricsReportingInterval = TimeSpan.FromSeconds(2.5);
-    static int EndpointSize = 25;
-    static int InstanceModulo = 4;
+    static TimeSpan MetricsReportingInterval = TimeSpan.FromSeconds(0.5);
+    static int EndpointSize = int.Parse(AppSettings["EndpointSize"]);
+    static int InstanceModulo = int.Parse(AppSettings["InstanceModulo"]);
     static int RecoverabilityImmediateRetryCount = 1;
     static int RecoverabilityDelayedRetryCount = 5;
     static TimeSpan RecoverabilityDelayedRetryBackoffIncrement = TimeSpan.FromSeconds(1);
-    static bool AuditForwardingEnabled = false;
+    static bool AuditForwardingEnabled = bool.TrueString.Equals(AppSettings["NServiceBus/AuditForwardingEnabled"], StringComparison.InvariantCultureIgnoreCase);
     static bool UseRandomHostId = false;
-    static LogLevel LogLevel = LogLevel.Error;
+    static LogLevel LogLevel = (LogLevel)Enum.Parse(typeof(LogLevel), AppSettings["NServiceBus/LogLevel"], true);
     static TimeSpan HeartbeatInterval = MetricsReportingInterval;
     static TimeSpan HeartbeatTTL = TimeSpan.FromTicks(HeartbeatInterval.Ticks * 4);
     static string HeartbeatQueue = "Particular.ServiceControl";
@@ -28,13 +35,16 @@ class Program
     public static (string name, IEndpointInstance instance)[] Instances;
     static async Task Main()
     {
+        InitAppDomainEventLogging();
+
+        GCSettings.LatencyMode = GCLatencyMode.Batch;
         if (Environment.UserInteractive) Console.Title = "Monitoring demo host";
 
         LogManager.Use<DefaultFactory>().Level(LogLevel);
 
         var providers = new[] { "Payment", "Audit", "Approval", "Ledger", "Integration", "Broadcast", "Notification", "Identity", "Report", "Exchange" };
         var types = new[] { "Service", "Gateway", "Adapter", "Translator", "Provider", "Generator" };
-        var ou = new[] { "Sales",  "Billing", "Shipping", "Transport", "Accounting", "Marketing", "Support", "Development", "Research", "HumanResourceManagement", "Production", "Purchasing" };
+        var ou = new[] { "Sales", "Billing", "Shipping", "Transport", "Accounting", "Marketing", "Support", "Development", "Research", "HumanResourceManagement", "Production", "Purchasing" };
 
         var random = new Random(1337); // Makes sure that random endpoints names are repeatable
 
@@ -43,7 +53,7 @@ class Program
         var tasks = new List<Task<(string name, IEndpointInstance)>>();
         for (int i = 0; i < EndpointSize; ++i)
         {
-            var endpointName = $"{ou[random.Next(ou.Length)]}.{providers[random.Next(providers.Length)]}{types[random.Next(types.Length)]}";
+            var endpointName = $"monitoringdemo.{ou[random.Next(ou.Length)]}.{providers[random.Next(providers.Length)]}{types[random.Next(types.Length)]}";
             W($"\tInitializing {endpointName}");
             for (int j = 0; j < i % InstanceModulo + 1; j++)
             {
@@ -53,6 +63,8 @@ class Program
             WL();
         }
 
+        UpdateConsoleTitle();
+
         WL("Starting...");
         Instances = await Task.WhenAll(tasks).ConfigureAwait(false);
         WL($"Done! Took {start.Elapsed} to start {Instances.Length} instances.");
@@ -60,6 +72,7 @@ class Program
 
         while (Console.ReadKey().Key != ConsoleKey.Escape)
         {
+            await Instances[0].instance.SendLocal(new SelfTest.Ping()).ConfigureAwait(false);
         }
 
         WL("Stopping...");
@@ -67,12 +80,22 @@ class Program
         WL("Stopped!");
     }
 
+     static async void UpdateConsoleTitle()
+    {
+        while (true)
+        {
+            Console.Title = $"{MyStartupTask.a:N0} = {MyStartupTask.b:N0}";
+            await Task.Delay(1000);
+        }
+    }
+
     static async Task<(string name, IEndpointInstance)> Create(string endpointName, int instanceNr)
     {
         var instanceSuffix = instanceNr.ToString("000");
         var cfg = new EndpointConfiguration(endpointName);
         //cfg.MakeInstanceUniquelyAddressable(instanceSuffix);
-        if (Debugger.IsAttached) cfg.EnableInstallers();
+        cfg.EnableInstallers();
+
         if (AuditForwardingEnabled) cfg.ForwardReceivedMessagesTo("audit");
         cfg.UsePersistence<InMemoryPersistence>();
         cfg.SendFailedMessagesTo("error");
@@ -87,42 +110,74 @@ class Program
             Environment.FailFast("NServiceBus CriticalError", ctx.Exception);
             return Task.CompletedTask;
         });
-        cfg.LimitMessageProcessingConcurrencyTo(4 * Environment.ProcessorCount);
+        cfg.LimitMessageProcessingConcurrencyTo(int.Parse(ConfigurationManager.AppSettings["NServiceBus/MaxConcurrency"], CultureInfo.InvariantCulture));
 
-        var transport = cfg.UseTransport<RabbitMQTransport>();
-        transport.ConnectionString("host=localhost");
-        transport.DelayedDelivery().DisableTimeoutManager();
+        //cfg.PurgeOnStartup(true);
+
+        var transportValue = System.Configuration.ConfigurationManager.AppSettings["Transport"];
+        Console.WriteLine(transportValue);
+        switch (transportValue)
+        {
+            case nameof(SqlServerTransport):
+                var sql = cfg.UseTransport<SqlServerTransport>();
+                sql.ConnectionStringName("Transport/SqlServer");
+                cfg.AssemblyScanner().ExcludeAssemblies("NServiceBus.Transports.RabbitMQ");
+                break;
+            case nameof(MsmqTransport):
+                var msmq = cfg.UseTransport<MsmqTransport>();
+                msmq.Transactions(TransportTransactionMode.ReceiveOnly);
+                //msmq.ConnectionStringName("Transport/MSMQ");
+
+                msmq.DisableDeadLetterQueueing();
+                //msmq.DisableConnectionCachingForSends();
+                ///msmq.UseNonTransactionalQueues();
+                //msmq.EnableJournaling();
+                //msmq.TimeToReachQueue(timespanValue);
+
+                cfg.AssemblyScanner().ExcludeAssemblies("NServiceBus.Transports.RabbitMQ");
+                break;
+            case nameof(RabbitMQTransport):
+                var transport = cfg.UseTransport<RabbitMQTransport>();
+                transport.ConnectionStringName("Transport/RabbitMQ");
+                //transport.DelayedDelivery().DisableTimeoutManager();
+                break;
+            default:
+                throw new NotSupportedException($"Unknown transport option '{transportValue}'");
+        }
+
+
+
+
         var pipeline = cfg.Pipeline;
-        pipeline.Register(
-            behavior: new RandomMessageTypeIncoming(),
-            description: nameof(RandomMessageTypeIncoming));
+        pipeline.Register(behavior: new RandomMessageTypeIncoming(), description: nameof(RandomMessageTypeIncoming));
 
-        pipeline.Register(
-            //behavior: new RateLimitBehavior(ThreadLocalRandom.Next(1, 10) * 100, TimeSpan.FromSeconds(10)),
-            behavior: new RateLimitBehavior(100, TimeSpan.FromSeconds(10)),
-            description: nameof(RateLimitBehavior));
+        //cfg.ApplyRateLimiting(500, TimeSpan.FromSeconds(10));
 
         var hostId = UseRandomHostId
             ? Guid.NewGuid()
             : GuidUtility.Create(NamespaceIdentifier, $"{endpointName}-{instanceSuffix}");
 
-#pragma warning disable CS0618 // Type or member is obsolete
-        cfg.EnableCriticalTimePerformanceCounter();
-#pragma warning restore CS0618 // Type or member is obsolete
-        cfg.UniquelyIdentifyRunningInstance().UsingCustomIdentifier(hostId);
-
-#pragma warning disable 618
-
         string domainName = IPGlobalProperties.GetIPGlobalProperties().DomainName;
         var fakeHostName = Dns.GetHostName() + instanceSuffix;
         fakeHostName += "." + domainName;
-        var instanceId = endpointName + "@" + fakeHostName;
+        var displayName = endpointName + "@" + fakeHostName;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        var performanceCounters = cfg.EnableWindowsPerformanceCounters();
+        performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromMinutes(1));
+#pragma warning restore CS0618 // Type or member is obsolete
+        var identification = cfg.UniquelyIdentifyRunningInstance();
+        //identification.UsingCustomIdentifier(hostId);
+        identification.UsingCustomDisplayName(displayName);
+
+#pragma warning disable 618
+
 
         var metrics = cfg.EnableMetrics();
         metrics.SendMetricDataToServiceControl(
             MonitoringQueue,
             MetricsReportingInterval
-            , instanceId
+            //, displayName
             );
 #pragma warning restore 618
 
@@ -142,9 +197,10 @@ class Program
         //    }
         //});
 
-        cfg.HeartbeatPlugin(HeartbeatQueue, HeartbeatInterval, HeartbeatTTL);
+        cfg.SendHeartbeatTo(HeartbeatQueue, HeartbeatInterval, HeartbeatTTL);
 
-        cfg.PurgeOnStartup(true);
+        //cfg.PurgeOnStartup(true);
+        //cfg.Conventions().DefiningMessagesAs(x => typeof(MyMessage).IsAssignableFrom(x));
         return (endpointName, await Endpoint.Start(cfg).ConfigureAwait(false));
     }
 
@@ -155,5 +211,21 @@ class Program
     static void W(string value = null)
     {
         Console.Out.WriteAsync(value).ConfigureAwait(false);
+    }
+
+    static void InitAppDomainEventLogging()
+    {
+        var firstChanceLog = LogManager.GetLogger("FirstChanceException");
+        var unhandledLog = LogManager.GetLogger("UnhandledException");
+        var domain = AppDomain.CurrentDomain;
+
+        domain.FirstChanceException += (o, ea) => { firstChanceLog.Debug(ea.Exception.Message, ea.Exception); };
+        domain.UnhandledException += (o, ea) =>
+        {
+            if (ea.ExceptionObject is Exception exception)
+            {
+                unhandledLog.Error(exception.Message, exception);
+            }
+        };
     }
 }
