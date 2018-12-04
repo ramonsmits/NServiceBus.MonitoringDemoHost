e2833368d4e0a96bd90e40ee89c64ba3424f4375
Ramon Smits <ramon.smits@gmail.com>
2018-12-04 13:23:07 +0100

    Added drop in plugins for monitoring v5 and v6 endpoints.

---
+++ MonitoringV5/AppConfigInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Linq;
+
+namespace NServiceBus.Metrics.ServiceControl
+{
+    class AppConfigInitializer : INeedInitialization
+    {
+        const string DefaultQueue = "Particular.Monitoring";
+        const string MonitoringDestinationKey = "ServiceControl/Monitoring/Address";
+
+        public void Customize(BusConfiguration configuration)
+        {
+            var log = NServiceBus.Logging.LogManager.GetLogger<AppConfigInitializer>();
+            var appSettings = ConfigurationManager.AppSettings;
+            var address = DefaultQueue;
+            var exists = appSettings.AllKeys.Contains(MonitoringDestinationKey);
+
+            log.InfoFormat("AppSetting '{0}' exist: {1}", MonitoringDestinationKey, exists);
+
+            if (exists) address = appSettings[MonitoringDestinationKey];
+
+            log.InfoFormat("Service control monitoring address: {0} ", address);
+            configuration.SendMetricDataToServiceControl(address);
+        }
+    }
+
+}
---
+++ MonitoringV6/EnableMonitoring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Metrics.ServiceControl;
+
+public class EnableMonitoring : INeedInitialization
+{
+    public void Customize(EndpointConfiguration cfg)
+    {
+        var metrics = cfg.EnableMetrics();
+        metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromSeconds(0.5));
+    }
+}
