bf33f8dcecfe03a7952f8553865b2f6af7f826e7
Ramon Smits <ramon.smits@gmail.com>
2018-12-04 13:20:27 +0100

    Parallel sending to increase throughput.

--- MonitoringDemoHost/SelfTestFeature.cs
+++ MonitoringDemoHost/SelfTestFeature.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Features;
 using NServiceBus.Logging;
+using SelfTest.Messages;
 
 namespace SelfTest
 {
@@ -21,25 +24,50 @@ namespace SelfTest
 
     class MyStartupTask : FeatureStartupTask
     {
+        public static long a;
+        public static long b;
+        private static long count;
+        private readonly long current = ++count;
         Task loop;
         bool stop;
 
-        protected override Task OnStart(IMessageSession session)
+        protected override async Task OnStart(IMessageSession session)
         {
             loop = Task.Run(() => Loop(session));
-            return Task.CompletedTask;
+            await Console.Out.WriteAsync($"{current}: Started").ConfigureAwait(false);
         }
+
+        static readonly TimeSpan SelfTestDelayBase = TimeSpan.Parse(System.Configuration.ConfigurationManager.AppSettings["SelfTest/DelayBase"], CultureInfo.InvariantCulture);
+        static readonly TimeSpan SelfTestDelayMin = TimeSpan.Parse(System.Configuration.ConfigurationManager.AppSettings["SelfTest/DelayMin"], CultureInfo.InvariantCulture);
+        static readonly TimeSpan SelfTestDelayMax = TimeSpan.Parse(System.Configuration.ConfigurationManager.AppSettings["SelfTest/DelayMax"], CultureInfo.InvariantCulture);
+
 
         async Task Loop(IMessageSession session)
         {
-            var min = ThreadLocalRandom.Next(10);
-            var max = ThreadLocalRandom.Next(min, 500);
+            var min = (int)SelfTestDelayBase.TotalMilliseconds + ThreadLocalRandom.Next((int)SelfTestDelayMin.TotalMilliseconds);
+            var max = ThreadLocalRandom.Next(min, (int)SelfTestDelayMax.TotalMilliseconds);
 
+            await Console.Out.WriteLineAsync($"{current}: {min}-{max},").ConfigureAwait(false);
+
+            var gate = new RateGate(10, TimeSpan.FromSeconds(1));
             while (!stop)
             {
-                session.SendLocal(new Ping()).ConfigureAwait(false);
-                int delay = ThreadLocalRandom.Next(min, max);
-                await Task.Delay(TimeSpan.FromMilliseconds(delay)).ConfigureAwait(false);
+                await gate.WaitToProceed()
+                    .ConfigureAwait(false);
+                Interlocked.Increment(ref a);
+                var send = Task.Run(async () =>
+                {
+                    try
+                    {
+                        await session.SendLocal(new Ping())
+                            .ConfigureAwait(false);
+                        Interlocked.Increment(ref b);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                });
             }
         }
 
@@ -51,7 +79,9 @@ namespace SelfTest
 
     }
 
-    class Ping : IMessage
+    class Ping :
+        //MyBase, MyMessage,
+        IMessage
     {
     }
 
@@ -64,4 +94,16 @@ namespace SelfTest
             return Task.CompletedTask;
         }
     }
+
+}
+namespace SelfTest.Messages
+{
+    public class MyBase : MyMessage
+    {
+    }
+
+    public interface MyMessage
+    {
+    }
+
 }
