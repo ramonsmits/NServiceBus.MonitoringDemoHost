de6879c2597fe962afb539e217d575d798d85659
Ramon Smits <ramon.smits@gmail.com>
2018-12-04 13:16:25 +0100

    Chaos factor configurable via config.

--- MonitoringDemoHost/ChaosHandler.cs
+++ MonitoringDemoHost/ChaosHandler.cs
@@ -1,20 +1,27 @@
 using System;
 using System.Collections.Concurrent;
+using System.Globalization;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
 
 class ChaosHandler : IHandleMessages<object>
 {
+    static readonly double ChaosFactor = double.Parse(System.Configuration.ConfigurationManager.AppSettings["ChaosFactor"], CultureInfo.InvariantCulture);
     readonly ILog Log = LogManager.GetLogger<ChaosHandler>();
-    readonly ConcurrentDictionary<string, double> Thressholds = new ConcurrentDictionary<string, double>();
+    static readonly ConcurrentDictionary<string, double> Thresholds = new ConcurrentDictionary<string, double>();
 
     public Task Handle(object message, IMessageHandlerContext context)
     {
         var enclosedMessageTypes = context.MessageHeaders[Headers.EnclosedMessageTypes];
-        var thresshold = Thressholds.GetOrAdd(enclosedMessageTypes, k => ThreadLocalRandom.NextDouble() * 0.05);
+        var threshold = Thresholds.GetOrAdd(enclosedMessageTypes, k =>
+        {
+            var chaosFactor = ThreadLocalRandom.NextDouble() * ChaosFactor;
+            Log.InfoFormat($"{0} chaos factor: {1*100:N}%",enclosedMessageTypes,chaosFactor);
+            return chaosFactor;
+        });
         var result = ThreadLocalRandom.NextDouble();
-        if (result < thresshold) throw new InvalidOperationException($"Random chaos ({thresshold * 100:N}% failure)");
+        if (result < threshold) throw new InvalidOperationException($"Random chaos ({threshold * 100:N}% failure)");
         return Task.FromResult(0);
     }
 }
