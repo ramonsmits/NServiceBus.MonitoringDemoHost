3a2ed969b8d8e77b73de41b61482c75f6955fd69
Ramon Smits <ramon.smits@gmail.com>
2018-12-04 13:17:07 +0100

    Delay configurable via config.

--- MonitoringDemoHost/DelayHandler.cs
+++ MonitoringDemoHost/DelayHandler.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Globalization;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
 
 class DelayHandler : IHandleMessages<object>
 {
+    static readonly TimeSpan DelayDurationMax = TimeSpan.Parse(System.Configuration.ConfigurationManager.AppSettings["DelayDurationMax"], CultureInfo.InvariantCulture);
     static readonly ILog Log = LogManager.GetLogger<DelayHandler>();
     readonly ConcurrentDictionary<string, int> durations = new ConcurrentDictionary<string, int>();
 
     public Task Handle(object message, IMessageHandlerContext context)
     {
         var enclosedMessageTypes = context.MessageHeaders[Headers.EnclosedMessageTypes];
-        int duration = durations.GetOrAdd(enclosedMessageTypes, k => ThreadLocalRandom.Next(2500));
+        int duration = durations.GetOrAdd(enclosedMessageTypes, k => ThreadLocalRandom.Next((int)DelayDurationMax.TotalMilliseconds));
         return Task.Delay(TimeSpan.FromMilliseconds(duration));
     }
 }
